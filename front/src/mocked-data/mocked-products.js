fbe6aeef70de8748e0f60ccf2ea82911ece11151
guilou <guilhemclr@gmail.com>
2025-01-16 16:15:46 +0100

    feat(front/ui): maid a basic ui to start the dev

---
+++ front/src/mocked-data/mocked-products.js
@@ -0,0 +1,17 @@
+export const mockedProducts = [
+	{
+		title: "Hoodie CoolStudent",
+		price: "39.99€",
+		description: "Le hoodie parfait pour vos soirées d'études",
+	},
+	{
+		title: "Pack de Survie Examens",
+		price: "24.99€",
+		description: "Tout ce dont vous avez besoin pour survivre aux examens",
+	},
+	{
+		title: "Mug Étudiant Caféiné",
+		price: "12.99€",
+		description: "Pour vos longues nuits de révision",
+	},
+];
