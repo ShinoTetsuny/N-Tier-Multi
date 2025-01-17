bf1c9c7f8cc344a2db60f6c11ec6d12b39d9cb71
guilou <guilhemclr@gmail.com>
2025-01-17 10:03:31 +0100

    fix(front/product-crud): changing put to patch in back office

--- back/services/products/routes/productsRoute.js
+++ back/services/products/routes/productsRoute.js
@@ -1,15 +1,15 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const productsController = require('../controllers/productsController');
-const authMiddleware = require('../middleware/authMiddleware');
+const productsController = require("../controllers/productsController");
+const authMiddleware = require("../middleware/authMiddleware");
 
 // Routes publiques
-router.get('/', productsController.getAllProducts);
-router.get('/:id', productsController.getProductById);
+router.get("/", productsController.getAllProducts);
+router.get("/:id", productsController.getProductById);
 
 // Routes protégées
-router.post('/', authMiddleware, productsController.createProduct);
-router.put('/:id', authMiddleware, productsController.updateProduct);
-router.delete('/:id', authMiddleware, productsController.deleteProduct);
+router.post("/", authMiddleware, productsController.createProduct);
+router.patch("/:id", authMiddleware, productsController.updateProduct);
+router.delete("/:id", authMiddleware, productsController.deleteProduct);
 
 module.exports = router;
